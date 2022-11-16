819a7a9949f8dde48c61fe001ab98688dfcd3436
perdasilva <perdasilva@redhat.com>
2022-11-16 14:16:26 +0100

    Add cli skeleton
    Signed-off-by: perdasilva <perdasilva@redhat.com>

---
+++ cmd/repo.go
@@ -0,0 +1,31 @@
+/*
+Copyright © 2022 Per G. da Silva <pegoncal@redhat.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// repoCmd represents the repo command
+var repoCmd = &cobra.Command{
+	Use:   "repo",
+	Short: "Manipulate software repositories",
+	Long:  `Add/Remove/List/Update software repositories from which to source content`,
+}
+
+func init() {
+	rootCmd.AddCommand(repoCmd)
+}
